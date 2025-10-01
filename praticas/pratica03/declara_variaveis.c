fc9cab292df1e165b4ffea749631d1d49ef1a0f3
Atsunoo <nicholas.pereira252@gmail.com>
2025-10-01 00:35:20 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()  {
+    int idade = 25;
+    printf("Idade: %i\n", idade);
+    return 0;
+}
---
+++ praticas/pratica03/faixa_tipo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main()  {
+    printf("O tipo 'double' aceita valores entre %E e %E.\n", DBL_MIN, DBL_MAX);
+    return 0;
+}
