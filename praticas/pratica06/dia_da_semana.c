981e14f2407b64c4c6f5ab17bd06f98486f0234e
unknown <2522130032@ACADEMICO.IESB.BR>
2025-10-22 10:51:40 -0300

    Adicionando os arquivos na pratica06

--- praticas/pratica06/dia_da_semana.c
+++ praticas/pratica06/dia_da_semana.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main ()  {
+    setlocale(LC_ALL, "");
+    int dia;
+    printf("Entre com um numero de 1 a 7: ");
+    scanf("%i", &dia);
+
+    switch (dia) {
+        case 1: printf("Domingo!\n", dia); break;
+        case 2: printf("Segunda!\n", dia); break;
+        case 3: printf("Terça!\n", dia); break;
+        case 4: printf("Quarta!\n", dia); break;
+        case 5: printf("Quinta!\n", dia); break;
+        case 6: printf("Sexta!\n", dia); break;
+        case 7: printf("Sábado!\n", dia); break;
+        default: printf("Número inválido, burro.");
+    }
+
     return 0;
 }
---
+++ praticas/pratica06/temperatura.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()  {  // > 40.0 muito quente. > 30.0 e < 40.0 quente. > 20.0 e < 30.0 agradável. < 20.0 frio.
+    float temperatura;
+    printf("Quantos graus ta fazendo na caminhada? ");
+    scanf("%f", &temperatura);
+
+    if (temperatura > 40.0f) {
+        printf("Ta pegando fogo bicho!")
+    } else if (valor_bruto <= 500.0f) {
+    } else {
+    }
+    return 0;
+}
