094263220e63bb368fe59d75e43180c2ff19dac1
Atsunoo <nicholas.pereira252@gmail.com>
2025-10-28 21:02:12 -0300

    terminando temperatura da pratica 06

--- praticas/pratica06/temperatura.c
+++ praticas/pratica06/temperatura.c
@@ -2,13 +2,18 @@
 
 int main ()  {  // > 40.0 muito quente. > 30.0 e < 40.0 quente. > 20.0 e < 30.0 agradável. < 20.0 frio.
     float temperatura;
-    printf("Quantos graus ta fazendo na caminhada? ");
+    printf("Quantos graus ta fazendo na caminhada?\n");
     scanf("%f", &temperatura);
 
     if (temperatura > 40.0f) {
-        printf("Ta pegando fogo bicho!")
-    } else if (valor_bruto <= 500.0f) {
-    } else {
+        printf("Ta pegando fogo bicho!\n");
+    } else if (temperatura >= 30.0f && temperatura <= 40.0f) {
+        printf("Ta quente...\n");
+    } if (temperatura >= 20.0f && temperatura < 30.0f) {
+        printf("Bem agradavel!\n");
+    } if (temperatura < 20.0f) {
+        printf("Ta um frio danado.\n");
     }
+
     return 0;
 }
